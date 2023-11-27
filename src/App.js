004953eb7e944edf154534483955bf0d34882e2d
cyrilmathewgeorge <cyrilsanthu@gmail.com>
2023-11-27 12:44:52 +0530

    Netflix React Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import {original, action, comedy, horror} from './urls'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Banner/>
+      <RowPost url={original} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={horror} title='Horror' isSmall />
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '567fcb84367e2901752187156c143bc6';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
